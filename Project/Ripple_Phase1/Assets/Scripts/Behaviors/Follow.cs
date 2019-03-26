9a7a021d006e0a5409446c5436944ea8d52bfc43
Alessandro <allezcheznice@gmail.com>
2019-03-26 22:44:34 +0100

    Camera tweak;

---
+++ Project/Ripple_Phase1/Assets/Scripts/Behaviors/Follow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+	//-----PUBLIC-----
+	//REFERENCES
+	[Header("References:")]
+	public Transform target;
+
+	[Space()]
+
+	//PARAMETERS
+	[Header("Parameters:")]
+	[Range(0, 1)] public float followSpeed = 1;
+	public Vector3 offset;
+	Transform self;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		self = transform;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+		self.position = Vector3.Lerp(self.position, target.position + offset, followSpeed);
+    }
+}
